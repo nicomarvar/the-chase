3af6a1d775a1867aa10f74a3cc0e3a0e48b66308
nicomarvar <nicomarvar@gmail.com>
2022-03-25 15:33:38 +0000

    uid installed

--- the-chase/lib/firebase/fbs-db-services.ts
+++ the-chase/lib/firebase/fbs-db-services.ts
@@ -1,15 +1,18 @@
 import { db } from "./fbs-config";
-import {addDoc, collection, getDocs} from "firebase/firestore"
+import {doc,setDoc, collection, getDocs} from "firebase/firestore"
+import { uid } from "uid";
 
 export async function getTest(){
     console.log("called")
+    const id = uid(8)
     // const usersRef = collection(db,"users");
     // const data = await (await getDocs(usersRef));
     // console.log(data.docs[0])
-    const docRef = await addDoc(collection(db, "users"), {
+    const docRef = await setDoc(doc(db, "users", id), {
         name: "Cali",
         country: "Colombia",
+        id: id
       });
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Document written with ID: ", id);
 
 }
